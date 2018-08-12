3054e80b849d4ef03395849ffca7e7587dd63400
sweeneyyy <sweeneyarnett@gmail.com>
2018-08-11 19:22:32 -0700

    events with drag and drop

--- src/components/DayScheduleContainer.js
+++ src/components/DayScheduleContainer.js
@@ -1,48 +1,50 @@
 import React, { Component } from 'react';
-import { ReactAgenda , ReactAgendaCtrl } from 'react-agenda';
+import { ReactAgenda , ReactAgendaCtrl, guid, getLast, getFirst } from 'react-agenda';
 import moment from 'moment';
 
 const colors = {
   "color-1": "rgb(46, 204, 113)",
   "color-2": "rgb(230, 126, 34)",
-  "color-3": "rgb(231, 76, 60)"
+  "color-3": "rgb(231, 76, 60)",
+  "color-4": "rgb(221, 160, 221)"
 }
 
 class DayScheduleContainer extends Component {
   constructor(props) {
     super(props);
 
-    const date = '2018-11-14T08:00:00Z';
+    const now = new Date();
+
     const initialEventData = [
       {
-        id: 1,
-        startTime: '2018-11-14T16:00:00Z',
-        endTime: '2018-11-14T18:00:00Z',
+        _id: guid(),
+        startDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0),
+        endDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 30),
         name: 'Morning Event',
         classes: "color-1"
-      }, {
-        id: 2,
-        startTime: '2018-11-14T20:00:00Z',
-        endTime: '2018-11-14T23:00:00Z',
+     }, {
+        _id: guid(),
+        startDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0),
+        endDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 30),
         name: 'Afternoon Event',
-        classes: "color-1"
+        classes: "color-4"
       }, {
-        id: 3,
-        startTime: '2018-11-15T06:00:00Z',
-        endTime: '2018-11-15T10:00:00Z',
+        _id: guid(),
+        startDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 0),
+        endDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 30),
         name: 'Evening Event',
         classes: "color-2"
       }, {
-        id: 3,
-        startTime: '2018-11-15T16:00:00Z',
-        endTime: '2018-11-15T18:00:00Z',
+        _id: guid(),
+        startDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 14, 0),
+        endDateTime: new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 16, 30),
         name: 'Next Day Event',
         classes: "color-3"
       }
     ];
 
     this.state = {
-      startDate: date,
+      startDate: new Date(),
       items: initialEventData,
       selected: [],
       cellHeight: 30,
@@ -52,6 +54,8 @@ class DayScheduleContainer extends Component {
       numberOfDays: 1
     }
     this.handleCellSelection = this.handleCellSelection.bind(this);
+    this.handleItemChange = this.handleItemChange.bind(this);
+    this.handleItemSize = this.handleItemSize.bind(this);
     this.handleItemEdit = this.handleItemEdit.bind(this);
     this.handleRangeSelection = this.handleRangeSelection.bind(this);
   }
@@ -61,21 +65,42 @@ class DayScheduleContainer extends Component {
   // }
   componentDidMount() {
     // this.setState({ items: items });
-    console.log(this.state.items);
-    console.log(this.state.startDate)
+    // console.log(this.state.items);
+    // console.log(this.state.startDate)
+  }
+
+  componentWillReceiveProps(next, last) {
+    if(next.items) {
+      this.setState({ items: next.items })
+    }
+  }
+
+  //drag and drop
+  handleItemChange(items, item) {
+    this.setState({ items: items })
+    console.log('handleItemChange', item)
+  }
+  //drag and resize
+  handleItemSize(items, item) {
+    this.setState({ items: items })
+    console.log('handleItemSize', item)
   }
 
   handleCellSelection(item) {
+    this.setState({ selected: [item] })
     console.log('handleCellSelection', item)
+  }
+
+  handleRangeSelection(selected) {
+    this.setState({ selected: selected})
+    console.log('handleRangeSelection', selected)
   }
 
   handleItemEdit(item) {
     console.log('handleItemEdit', item)
   }
 
-  handleRangeSelection(item) {
-    console.log('handleRangeSelection', item)
-  }
+
 
   render() {
     const {
@@ -86,8 +111,13 @@ class DayScheduleContainer extends Component {
     const maxDate = moment(startDate).add(2, 'd');
 
     const AgendaItem = function(props) {
-      console.log('item component props' , props);
-      return <div style={{display: 'block', position: 'absolute', background: '#FFF'}}>{props.item.name}</div>
+      const startHour = moment(props.item.startDateTime).format("h:mm");
+      const endHour = moment(props.item.endDateTime).format("h:mm");
+
+      return <div style={{display: 'block', position: 'absolute'}}>
+        <h3>{props.item.name}</h3>
+        <h4>{startHour} to {endHour}</h4>
+      </div>
     }
 
     return (
@@ -105,6 +135,7 @@ class DayScheduleContainer extends Component {
         itemColors={colors}
         autoScale={false}
         fixedHeader={true}
+        onChangeEvent={this.handleItemChange}
         onItemEdit={this.handleItemEdit}
         onCellSelect={this.handleCellSelection}
         onRangeSelection={this.handleRangeSelection} />
